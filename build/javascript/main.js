309cb234dac745563c10b6d0da47bb85d845c6ad
eberwild <kevinflotow@arcor.de>
2026-03-23 16:00:42 +0100

    TypeScript Setup

---
+++ build/javascript/main.js
@@ -0,0 +1,7 @@
+let a = 12;
+let b = 4;
+let c = 3;
+console.log(a / b);
+console.log(c * b);
+export {};
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+let a: number = 12;
+let b: number = 4;
+let c: number = 3;
+
+console.log(a/b)
+console.log(c*b)
