c01c1c8c316175f2b9bdcb4b8f942aa8d7bec539
eberwild <kevinflotow@arcor.de>
2026-03-24 01:12:48 +0100

    types , interfaces , enums

--- build/javascript/main.js
+++ build/javascript/main.js
@@ -1,7 +1,58 @@
-let a = 12;
-let b = 4;
-let c = 3;
-console.log(a / b);
-console.log(c * b);
+//simple type declaration
+let myName = 'Peter';
+// union typ
+let album;
+// any typ kills typescript
+let age;
+// function with type safe arguments and output
+const Sum = (a, b) => {
+    return a + b;
+};
+// Arrays
+let namesArray = ['luffy', 'nami', 'zoro'];
+let mixedArray = [42, 'Spandam', false];
+// Tuple -> more strict (number of items and order matter)
+let Tuple = ['peter', 42, true];
+// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+const User = {
+    userName: 'eberwild',
+    userEmail: 'myNotWorkingArcorEmail',
+    age: 31,
+    hasPremium: true,
+};
+const luffy = {
+    name: 'luffy',
+    crewName: 'strawhats',
+    role: 'captain',
+    bounty: 300000000,
+    //active: true,
+    crew: []
+};
+const getBounty = (pirate) => {
+    return pirate.bounty;
+};
+const garp = {
+    name: 'garp',
+    rang: 'vice-admiral',
+    active: true,
+    crew: []
+};
+const getRang = (marine) => {
+    return marine.rang;
+};
+// Enums -> every value enumareted
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    // all following values will adapted 
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A);
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,79 @@
-let a: number = 12;
-let b: number = 4;
-let c: number = 3;
+//simple type declaration
+let myName: string = 'Peter';
+// union typ
+let album: string | number;
+// any typ kills typescript
+let age: any;
+// function with type safe arguments and output
+const Sum = (a:number,b:number):number => {
+    return a+b;
+}
+// Arrays
+let namesArray: string[] = ['luffy' , 'nami' , 'zoro'];
+let mixedArray: (string | number | boolean)[] = [42 , 'Spandam' , false];
 
-console.log(a/b)
-console.log(c*b)
+// Tuple -> more strict (number of items and order matter)
+let Tuple: [string , number , boolean] = ['peter' , 42 , true]
+
+// Objects
+let myObj : object;
+myObj = [];
+console.log(typeof myObj);
+
+const User = {
+    userName: 'eberwild',
+    userEmail: 'myNotWorkingArcorEmail',
+    age: 31,
+    hasPremium: true,
+}
+// type annotations 
+type Pirate = {
+    name: string,
+    crewName: string,
+    role: string,
+    bounty: number,
+    // optional property (either boolean | undefined)
+    active?: boolean,
+    crew: Pirate[]
+}
+const luffy: Pirate = {
+    name: 'luffy',
+    crewName: 'strawhats',
+    role: 'captain',
+    bounty: 300000000,
+    //active: true,
+    crew: []
+}
+const getBounty = (pirate: Pirate) => {
+    return pirate.bounty;
+}
+
+// interfaces -> better for an object structure (inheritance etc.)
+//            -> cannot use union types
+interface Marine {
+    name: string,
+    rang: string,
+    active: boolean,
+    crew: Marine[]
+}
+const garp: Marine = {
+    name: 'garp',
+    rang: 'vice-admiral',
+    active: true,
+    crew: []
+}
+const getRang = (marine: Marine) => {
+    return marine.rang;
+}
+
+// Enums -> every value enumareted
+enum Grade {
+    U = 1,
+    // all following values will adapted 
+    D,
+    C,
+    B,
+    A
+}
+console.log(Grade.A)
+
